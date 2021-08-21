799b6eafd0ca61e737c61c6ff81e39daea5104d7
Aryan <test@test.com>
2021-08-21 18:51:12 +0530

    login pending

---
+++ public/js/userLogin.js
@@ -0,0 +1,25 @@
+function login() {
+    const name = document.getElementById("name").value;
+    console.log(document.getElementById("name").value)
+
+    var text = new Text({
+        name: name
+    })
+    if (name === '') {
+        alert('input some message');
+        return false;
+        } else{
+            text = inputValue;
+        }
+    // .then(function (response) {
+    //     if (response) {
+    //     return response;
+    //     }
+    // })
+    // .catch(function (error) {
+    //     console.log(error);
+    // });
+}
+
+
+login()
