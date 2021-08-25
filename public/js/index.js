3e05f51fe89be972faf0d0eba21a82b26e455728
Aryan <test@test.com>
2021-08-25 12:10:54 +0530

    login js

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+function logOut() {
+      localStorage.removeItem("name");
+      location.replace("/home/celticlab/Desktop/Whats-App/views/userLogin.html")
+}
--- public/js/userLogin.js
+++ public/js/userLogin.js
@@ -1,25 +1,27 @@
 function login() {
-    const name = document.getElementById("name").value;
+    localStorage.setItem("name", document.getElementById("name").value)
     console.log(document.getElementById("name").value)
-
-    var text = new Text({
-        name: name
-    })
-    if (name === '') {
-        alert('input some message');
-        return false;
-        } else{
-            text = inputValue;
-        }
-    // .then(function (response) {
-    //     if (response) {
-    //     return response;
-    //     }
-    // })
-    // .catch(function (error) {
-    //     console.log(error);
-    // });
+    location.replace("/home/celticlab/Desktop/Whats-App/views/index.html")
 }
 
+//     var text = new Text({
+//         name: name
+//     })
+//     if (name === '') {
+//         alert('input some message');
+//         return false;
+//         } else{
+//             text = inputValue;
+//         }
+//     // .then(function (response) {
+//     //     if (response) {
+//     //     return response;
+//     //     }
+//     // })
+//     // .catch(function (error) {
+//     //     console.log(error);
+//     // });
+// }
 
-login()
+
+// login()
