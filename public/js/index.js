a0143aec8fdcc633fc3d453569fbe917b7ac9389
aryan21999 <itsaryangupta1999@gmail.com>
2021-09-03 13:46:27 +0530

    some update in function

--- public/js/index.js
+++ public/js/index.js
@@ -4,17 +4,47 @@ var list = ''
 console.log(list)
 
 for (i = 0; i < friends.length; i++) {
-    console.log(friends[i])
-    list += `<button class="card" id="${i}" onclick="openClick(event)"><h4>${friends[i]}</h4></button><br>`
+    list += `<div class=row-8> 
+    <button class="card" id="${i}" onclick="openClick(event)"  value="${friends[i]}">${friends[i]}</button><br>
+    </div>`
 }
 
+document.getElementById('chatHeader').innerHTML = friends[0]
+document.getElementById('friends').innerHTML = list
+
 function openClick(e) {
-    var i, card;
-    friends = document.getElementById("friends");
+    var i, card, chatcontent;
     card = document.getElementsByClassName("card");
     for (i = 0; i < card.length; i++) {
         card[i].className = card[i].className.replace(" active", "");
     }
+    chatcontent = document.getElementsByClassName("chatcontent");
+    for (i = 0; i < chatcontent.length; i++) {
+      chatcontent[i].style.display = "none";
+    }
+    document.getElementById('chat').innerHTML = list
+
     e.currentTarget.className += " active";
+
+    document.getElementById('chatHeader').innerHTML = e.currentTarget.getAttribute('value')
 }
-document.getElementById('friends').innerHTML = list
+
+
+
+var chat = JSON.parse(localStorage.getItem('chat'))
+var list = ''
+console.log(list)
+
+for (i=0; i< chat.length; i++)
+{
+    list += `<div class="chat" id="${i}"><p>${chat[i]}</p></div>`
+}
+
+var chat = [["Hlw bhai"],["hi"],["kya kr rhe"],["kuch nahi."]]
+
+localStorage.setItem("chat", JSON.stringify(chat))
+
+
+
+
+
